addc1a7e4482896254c9e42c47c3fb99fa171fe5
elijah <elireid.28@gmail.com>
2024-02-16 15:10:27 -0500

    first commit on branch

--- RPG V1.0/obj/Debug/net8.0/RPG V1.0.AssemblyInfo.cs
+++ RPG V1.0/obj/Debug/net8.0/RPG V1.0.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPG V1.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51511f297b484761d6ad1e801cad8be27d8f6cc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d52879f4c5161e5037e0bcc7e49bee60a0ba82a")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPG V1.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPG V1.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
